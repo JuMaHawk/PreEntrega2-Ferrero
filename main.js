c78bebdeec3938f4c003f52ff044cc6dea6e91de
JuMaHawk <jmferrero@gmail.com>
2023-10-17 03:08:03 -0300

    Mejorando la funcion de eliminar productos

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
 const productos = [];
-
 function eliminarProducto(productoEliminado) {
+
     const indice = productos.indexOf(productoEliminado);
-    productos.splice(indice, 1);
-    console.log(indice)
+
+    if (productos.length <= 0) {
+
+        pedido = prompt(`No hay productos agregados en la lista\nSi desea agregar un producto a la lista de compras ingrese "A"\nSi desea salir ingrese "SALIR"`)
+        pedido = pedido.toUpperCase();
+
+    } else {
+        productos.splice(indice, 1);
+        if (indice === -1) {
+            pedido = prompt(`El producto ingresado no se encuentra en la lista.\nPor favor ingrese el producto que desea eliminar de la lista\n` + productos);
+            
+        } else {
+            alert("Listado de compras:\n" + productos.join("\n"))
+            pedido = prompt(`Si desea agregar un producto a la lista de compras ingrese "A"\nSi desea eliminar un producto ingrese "E"\nSi desea salir ingrese "SALIR"`);
+            pedido = pedido.toUpperCase();
+        }
+    }
 }
 
-let pedido = prompt(`Si desea agregar un producto a la lista de compras ingrese "A", si desea eliminar un producto ingrese "E", si desea salir ingrese "SALIR"`);
+let pedido = prompt(`Si desea agregar un producto a la lista de compras ingrese "A"\nSi desea eliminar un producto ingrese "E"\nSi desea salir ingrese "SALIR"`);
 pedido = pedido.toUpperCase();
 
-while (pedido != "SALIR"){
+while (pedido != "SALIR") {
 
-if (pedido == "A") {
-    const nuevoProducto = prompt("Ingrese el producto que desea agregar a la lista");
-    productos.unshift(nuevoProducto)
-    alert(productos)
-    pedido = prompt(`Si desea agregar un producto a la lista de compras ingrese "A", si desea eliminar un producto ingrese "E", si desea salir ingrese "SALIR"`);
-    pedido = pedido.toUpperCase();
-} else if (pedido == "E") {
-    const productoEliminado = prompt("Ingrese el producto que desea eliminar")
-    eliminarProducto(productoEliminado)
-    alert(productos)
-    pedido = prompt(`Si desea agregar un producto a la lista de compras ingrese "A", si desea eliminar un producto ingrese "E", si desea salir ingrese "SALIR"`);
-    pedido = pedido.toUpperCase();
-} else {
-    pedido = prompt(`Por favor ingrese un valor existente.
-    Si desea agregar un producto a la lista de compras ingrese "A", si desea eliminar un producto ingrese "E", si desea salir ingrese "SALIR"`);
-    pedido = pedido.toUpperCase();
-}
+    if (pedido == "A") {
+        const nuevoProducto = prompt("Ingrese el producto que desea agregar a la lista");
+        productos.push(nuevoProducto)
+        alert("Listado de compras:\n" + productos.join("\n"))
+        pedido = prompt(`Si desea agregar un producto a la lista de compras ingrese "A"\nSi desea eliminar un producto ingrese "E"\nSi desea salir ingrese "SALIR"`);
+        pedido = pedido.toUpperCase();
+    } else if (pedido == "E") {
+        const productoEliminado = prompt("Ingrese el producto que desea eliminar")
+        eliminarProducto(productoEliminado)
+
+    } else {
+        pedido = prompt(`Por favor ingrese un valor existente.\nSi desea agregar un producto a la lista de compras ingrese "A"\nSi desea eliminar un producto ingrese "E"\nSi desea salir ingrese "SALIR"`);
+        pedido = pedido.toUpperCase();
+    }
 }
 
-/* let entrada = prompt ("ingrese algo");
-
-while (entrada != "SALIR"){
-     alert(`El usuario ingreso ${entrada}`);
-
-     entrada = prompt("Ingresar dato ")
-} */
-
-//Esto es nuevo.
